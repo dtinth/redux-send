99084e7a89528003008fd54760bc3ae815053b72
Thai Pangsakulyanont <dtinth@spacet.me>
2016-03-12 14:26:52 +0700

    Update 2016-03-12T07:26:47.994Z

--- demo/src/counter/CounterModel.js
+++
@@ -1,8 +0,0 @@
-
-const CounterModel = {
-  getInitialState: () => 0,
-  increment: () => state => state + 1,
-  decrement: () => state => state - 1
-}
-
-export default CounterModel
--- demo/src/reducer.js
+++
@@ -1,6 +0,0 @@
-
-import { createReducer } from '../../src'
-import AppModel from './app/AppModel'
-
-export const reducer = createReducer(AppModel)
-export default reducer
